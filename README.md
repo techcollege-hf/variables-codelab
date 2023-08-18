a887a5aa1304b3fa7c988ea346499097bc1f0531
bo-nicolaisen <bni@techcollege.dk>
2023-08-18 09:45:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# template-codeLab
+   Opgaver og forståelse
+   
+   promt 
+   skriv en kort beskrivelse, med eksempler, om template strings i javascript. på dansk. så forklaringen kan ses på en web side, med kode eksempler
---
+++ opgavefiler/js/medium.js
@@ -0,0 +1,22 @@
+/* Opgave 1*/
+
+let myBall=document.getElementById('ballOne');
+let myBuhButton=document.getElementById('buhOneButton');
+
+
+
+
+/*opgave 2*/
+
+let myBalls=document.getElementsByClassName('balls');
+let myBuhTwoButton=document.getElementById('buhTwoButton');
+
+
+
+/*opgave 3*/
+
+
+
+
+
+
